83b8073d96b11e19ff368c4d58a7bf518d4a22f9
Andrey Ivanov <andrey.i@reviewapp.today>
2023-11-15 08:19:08 +0500

    invisible execution

--- windows/start.bat
+++ windows/start.bat
@@ -1,4 +1,4 @@
-set SCRIPT_PATH=c:\python_project\server_monitoring\windows
+set SCRIPT_PATH=C:\service\server_monitoring-main\windows
 cd  %SCRIPT_PATH%
 
 venv\Scripts\python.exe %SCRIPT_PATH%\cpu_temp.py
---
+++ windows/start.vbs
@@ -0,0 +1,2 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run "cmd.exe /c C:\service\server_monitoring-main\windows\start.bat", 0
